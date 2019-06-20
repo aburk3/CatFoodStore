9e5858e5f14ad2dcc8269099d0384a980fbbbe66
aburk3 <aburk3@gmail.com>
2019-06-20 05:34:09 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Cat_Food_Store
+{
+  class Program
+  {
+
+    static void Ask(string question)
+    {
+      Console.WriteLine(question);
+      string answer = Console.ReadLine();
+      Console.WriteLine(answer);
+    }
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Welcome to the cat food store!");
+      Ask("How many cans are you ordering?");
+    }
+  }
+}
