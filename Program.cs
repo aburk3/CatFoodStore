2732f5a9b2b24bca0f1f3916769c9d86eb774a41
aburk3 <aburk3@gmail.com>
2019-06-20 06:07:58 -0500

    Using interpolation instead of concatenation

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ namespace Cat_Food_Store
     {
       Console.WriteLine("Welcome to the cat food store!");
       string entry = Ask("How many cans are you ordering?");
-      Console.WriteLine("You entered " + entry + " cans");
+      Console.WriteLine($"You entered {entry} cans");
     }
   }
 }
